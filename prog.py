14ed358112c139f2b7495b7dc82ef77d9cd1dae0
Arsenii Zhalnin <mrzhalnin@gmail.com>
2023-04-19 08:40:55 +0300

    Add cases

--- prog.py
+++ prog.py
@@ -24,5 +24,29 @@ def save_notes(notes):
     with open(file_path, 'w', encoding='utf8') as open_book:
         json.dump(notes, open_book, indent=4)
 
+def tasks(task):
+   if task > 5: print('Вы ошиблись')
+   if task == 5: print('До свидания!')
+   else:
+    match task:
+        case 1: ## Добавить заметку
+            ##TBA
+            print_menu()
+            tasks(int(input('Введите номер задачи от 1 до 5: ')))   
+        case 2: ## Вывести все заметки
+            ##TBA
+            print_menu()
+            tasks(int(input('Введите номер задачи от 1 до 5: ')))
+        case 3: ## Найти и отредактировать заметку
+            ##TBA
+            print_menu()
+            tasks(int(input('Введите номер задачи от 1 до 5: ')))
+        case 4: ## Удалить заметку
+            ##TBA
+            print_menu()
+            tasks(int(input('Введите номер задачи от 1 до 5: ')))          
+        case _:
+            print('Введите значение из меню: ')
+            tasks(int(input('Введите номер задачи от 1 до 5: ')))
 
 file_path = "notebook.json"
